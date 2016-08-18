1c22788960ddef31f4f0608c8cdf5673e3f5cf2a
DoXuanLinh <Doxuanlinhit2.3@gmail.com>
2016-08-18 09:53:45 +0700

    tets

---
+++ demoTileMap/Assets/collision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collision : MonoBehaviour {
+	void Update(){
+		gameObject.transform.position += new Vector3 (-0.1f, 0f, 0f);
+	}
+	// Use this for initialization
+	void OnCollisionEnter(Collision coll){
+		Debug.Log ("aaaaaa");
+		if (coll.gameObject.name=="Collision") {
+			Destroy (gameObject);
+		}
+	}
+}
